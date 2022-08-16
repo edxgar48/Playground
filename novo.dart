f27297106663442058a8c0894b5c3eeb96f61cde
edxgar48 <edxgar48@gmail.com>
2022-08-16 16:32:52 -0300

    Alterei todoss os comentarios dexei tudo BR

--- novo.dart
+++ novo.dart
@@ -2,20 +2,22 @@ void main() {
   final myString =
       'https://us06web.zoom.us/j/80302474263?pwd=ZYhEdHExMHovZ1YvaDNjXnBnTFh2UT09';
 
-// find a variable length hex value at the beginning of the line
+//pega a primeira REGEX com 11 digitos sequenciais
   final regexp = RegExp(r'(\d{11})');
-  final regexp_2 = RegExp(r'(?:=/=................................)');
+//pega a segunda REGEX que casa tudo depois do = (até o =)
+  final regexp_2 =
+      RegExp(r'=([\s\S]*)$'); //preciso aprender a negar o = aqui r'=([\s\S]*)$'
+  // talvés algo assim r'[^=](*$)'
 
-// find the first match though you could also do `allMatches`
+//casa as REGEX
   final match = regexp.firstMatch(myString);
   final match_2 = regexp_2.firstMatch(myString);
 
-// group(0) is the full matched text
-// if your regex had groups (using parentheses) then you could get the
-// text from them by using group(1), group(2), etc.
-  late final matchedText = match?.group(0); // 25F8
+// group(0) é todo casamento
+
+  late final matchedText = match?.group(0);
   late final matchedText_2 = match_2?.group(0);
 
-  print('Login: $matchedText');
-  print('Senha : $matchedText_2');
+  print('Login: $matchedText'); //Login: 80302474263
+  print('Senha : $matchedText_2'); //Senha : =ZYhEdHExMHovZ1YvaDNjXnBnTFh2UT09
 }
