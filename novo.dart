a276043e509a57d7267c4fbfb3f7546f7444ff84
edxgar48 <edxgar48@gmail.com>
2022-08-16 16:04:14 -0300

    REGEX melhores e com grupos

--- novo.dart
+++ novo.dart
@@ -4,14 +4,18 @@ void main() {
 
 // find a variable length hex value at the beginning of the line
   final regexp = RegExp(r'(\d{11})');
+  final regexp_2 = RegExp(r'(?:=/=................................)');
 
 // find the first match though you could also do `allMatches`
   final match = regexp.firstMatch(myString);
+  final match_2 = regexp_2.firstMatch(myString);
 
 // group(0) is the full matched text
 // if your regex had groups (using parentheses) then you could get the
 // text from them by using group(1), group(2), etc.
   late final matchedText = match?.group(0); // 25F8
+  late final matchedText_2 = match_2?.group(0);
 
   print('Login: $matchedText');
+  print('Senha : $matchedText_2');
 }
