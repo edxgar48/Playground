d838de5dd6938c3f55613d74e27676a04b9726c8
edxgar48 <edxgar48@gmail.com>
2022-08-17 09:52:33 -0300

    Agora negando o = na REGEX só mudei o group

--- novo.dart
+++ novo.dart
@@ -16,8 +16,8 @@ void main() {
 // group(0) é todo casamento
 
   late final matchedText = match?.group(0);
-  late final matchedText_2 = match_2?.group(0);
+  late final matchedText_2 = match_2?.group(1); //só mudei o group(1)e negou o =
 
   print('Login: $matchedText'); //Login: 80302474263
-  print('Senha : $matchedText_2'); //Senha : =ZYhEdHExMHovZ1YvaDNjXnBnTFh2UT09
+  print('Senha : $matchedText_2'); //Senha : ZYhEdHExMHovZ1YvaDNjXnBnTFh2UT09
 }
